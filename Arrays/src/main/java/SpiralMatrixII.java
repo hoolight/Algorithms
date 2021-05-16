99e35e9629072c994d64d8537e1cf60b252317d1
HooL <chinahool@126.com>
2021-05-16 20:56:40 +0800

    螺旋矩阵1、2

--- Arrays/src/main/java/SpiralMatrixII.java
+++ Arrays/src/main/java/SpiralMatrixII.java
@@ -1,5 +1,48 @@
+import java.util.Arrays;
+
 /**
  * 螺旋矩阵2
  */
 public class SpiralMatrixII {
+    public static int[][] generateMatrix(int n) {
+        if (n==0){
+            throw new IllegalArgumentException("No solution");
+        }
+        int[][] result=new int[n][n];
+        int left = 0;
+        int right = n-1;
+        int top = 0;
+        int bottom = n-1;
+        int num = 1;
+        while (left<=right&&top<=bottom){
+            for (int i = left; i <= right; i++) {
+                result[top][i] = num++;
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result[i][right] = num++;
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                result[bottom][i] = num++;
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                result[i][left] = num++;
+            }
+            left++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        Arrays.stream(SpiralMatrixII.generateMatrix(n)).forEach(i->{
+            System.out.print("[");
+            Arrays.stream(i).forEach(j->{
+                System.out.print(j+",");
+            });
+            System.out.printf("]");
+        });
+    }
 }
